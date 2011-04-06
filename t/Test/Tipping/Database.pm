f75bcbe61528e57a54e658b5955ae0a82c919c6f
Stephen Thirlwall <stephent@strategicdata.com.au>
2011-04-06 17:46:36 +1000

    Added class to create temporary databases for test

---
+++ t/Test/Tipping/Database.pm
@@ -0,0 +1,90 @@
+package Test::Tipping::Database;
+use Modern::Perl;
+
+use Carp            qw/ croak /;
+
+sub install {
+    my ($driver, $config) = @_;
+
+    my $dsn;
+
+    given ($driver) {
+
+        when ('Pg') {
+            use Test::postgresql ();
+            $config->{test_database} = Test::postgresql->new()
+                or croak $Test::postgresql::errstr;
+
+            # STFU please postgres...
+            my $dbh = DBI->connect($config->{test_database}->dsn);
+            $dbh->do(q(ALTER USER postgres SET Client_min_messages='warning'))
+                or croak $dbh->errstr;
+        }
+
+        when ('mysql') {
+            use Test::mysqld ();
+            $config->{test_database} = Test::mysqld->new(
+                    my_cnf => { 'skip-networking' => '' }
+                    ) or croak $Test::mysqld::errstr;
+        }
+
+        when ('SQLite') {
+            $dsn = q(dbi:SQLite:dbname=:memory:);
+        }
+
+        default {
+            die 'Expecting one of mysql, Pg, or SQLite';
+        }
+    }
+
+    $config->{database}->{dsn} = $dsn // $config->{test_database}->dsn;
+    delete $config->{database}->{user};
+    delete $config->{database}->{password};
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Test::Tipping::Database - Create temporary databases for tests
+
+=head1 DESCRIPTION
+
+Creates temporary database instances for use during testing. Currently
+supports PostgreSQL, MySQL and SQLite.
+
+PostgreSQL uses Test::postgresqld and MySQL uses Test::mysqld. SQLite uses
+the built-in :memory: database.
+
+The database is created and the database config attributes are written into
+the supplied config hash.
+
+=head1 SYNOPSIS
+
+  use Tipping::Config;
+  use Test::Tipping::Database;
+
+  my $db_driver = 'Pg';
+  Test::Tipping::Database::install($db_driver, Tipping::Config->config);
+
+=head1 FUNCTIONS
+
+=head2 install ($driver_name, $config_hash)
+
+Creates a temporary database for the given database type, and modifies
+$config_hash->{database}->{qw/ dsn user password } to use that database.
+
+The temporary database object is written to $config_hash->{test_database} in
+order to keep the database in scope. The database closes when the config hash
+goes out of scope.
+
+=head1 AUTHOR
+
+Stephen Thirlwall <sdt@dr.com>
+
+
+=cut
