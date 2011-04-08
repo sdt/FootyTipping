d04597dbb2280d307b0cf01a3cf568d069309339
Stephen Thirlwall <stephent@strategicdata.com.au>
2011-04-08 13:33:22 +1000

    Added automated_test.sh

---
+++ automated_test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Automated testing
+
+# Do the basic tests first
+for db in mysql Pg SQLite; do
+    echo Testing with $db
+    TIPPING_DB_DRIVER=$db prove -wl t
+done
+
+# Then do another run with TEST_POD and TEST_CRITIC
+TIPPING_DB_DRIVER=SQLite TEST_POD=1 TEST_CRITIC=1 prove -wl t
+
+# Finally, do a Devel::Cover run
+perl Makefile.PL
+cover -delete
+HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,^$HOME/perl5,^t/ make test
+cover
+make distclean
