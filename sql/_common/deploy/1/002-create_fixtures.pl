98ba4b0b70934717bdb533d9b7c90e07b1953c14
Stephen Thirlwall <sdt@dr.com>
2011-04-10 16:03:42 +1000

    Trivial layout changes

--- sql/_common/deploy/1/002-create_fixtures.pl
+++ sql/_common/deploy/1/002-create_fixtures.pl
@@ -6,11 +6,11 @@ use Tipping::Populator ();
 sub {
     my $schema = shift;
 
-    for my $yaml (qw{ teams venues 2011/games 2011/results}) {
+    for my $yaml (qw{ teams venues 2011/games 2011/results }) {
         Tipping::Populator::populate(
             schema  => $schema,
             yaml    => scalar slurp("yaml/$yaml.yml"),
-#            verbose => 1,
+#           verbose => 1,
         );
     }
 }
