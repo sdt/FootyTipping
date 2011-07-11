3c10905a9a4676614b9f58cb0d8d8ba8d7936141
Stephen Thirlwall <sdt@dr.com>
2011-07-11 17:28:45 +1000

    Added 'no indirect' to Modern::Perl::5_14

--- lib/Modern/Perl/5_14.pm
+++ lib/Modern/Perl/5_14.pm
@@ -1,20 +1,22 @@
 package Modern::Perl::5_14;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 use 5.014_000;
 
 use strict;
 use warnings;
 
-use mro     ();
-use feature ();
+use mro      ();
+use feature  ();
+use indirect ();
 
 sub import {
     warnings->import();
     strict->import();
     feature->import( ':5.14' );
     mro::set_mro( scalar caller(), 'c3' );
+    indirect->unimport();
     return;
 }
 
@@ -29,12 +31,14 @@ Modern::Perl::5_14 - Modern::Perl for 5.14
 
 =head1 VERSION
 
-Version 1.00
+Version 1.01
 
 =head1 DESCRIPTION
 
 Modern::Perl for 5.14. Straight copy from chromatic's version with the tens
 changed to fourteens.
+
+Update: added 'no indirect'
 
 =head1 SYNOPSIS
 
@@ -46,7 +50,7 @@ Stephen Thirlwall <sdt@dr.com>
 
 =head1 ACKNOWLEDGEMENTS
 
-chromatic, C<< <chromatic at wgz.org> >> wrote Modern::Perl.
+chromatic C<< <chromatic at wgz.org> >> wrote Modern::Perl.
 I just changed some zeros to fours.
 
 =head1 LICENSE
